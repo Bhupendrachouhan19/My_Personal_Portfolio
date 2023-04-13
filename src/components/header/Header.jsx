8f8df0988d08743c2a7508eee77da0ebefadb1e7
Bhupendrachouhan19 <iiitianbhupendra@gmail.com>
2023-04-13 10:15:03 +0530

    Header Component's JSX and Styling is Done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
     <header>
-      <div className="container header__container"></div>
+      <div className="container header__container">
+        <h5>Hii I'm,</h5>
+        <h1>Bhupendra Chouhan</h1>
+        <CTA />
+
+        {/* My Photo */}
+        <div><img src={ME} className="me" alt="Bhupendra Chouhan" /></div>
+
+      
+        <a href="#contact" className='scroll__down'>Scroll Down</a>        
+        <HeaderSocials />
+      </div>
     </header>
   )
 }
