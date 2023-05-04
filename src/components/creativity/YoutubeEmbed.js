8c6776e36d4101f24bd4f72f42488be77060b556
Bhupendrachouhan19 <iiitianbhupendra@gmail.com>
2023-05-04 08:04:51 +0530

    Creative Component's YT video made responsive

--- src/components/creativity/YoutubeEmbed.js
+++ src/components/creativity/YoutubeEmbed.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './youtubeEmbed.css'
+
 const YoutubeEmbed = ({embedId}) => {
   return (
-    <div>
+    <div className="yt_responsive">
 <iframe width="560" height="315" src="https://www.youtube.com/embed/sNMQ5xwFRlg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   )
