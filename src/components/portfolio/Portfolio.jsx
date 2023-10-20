91c0927af234989773b136fc93ad0963c613e0b6
Bhupendrachouhan19 <iiitianbhupendra@gmail.com>
2023-10-20 06:21:19 +0530

    Minor Changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,18 +6,19 @@ import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.png'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.png'
+import IMG7 from '../../assets/portfolio7.png'
 
 
 // Portfolio Items Data:
 const portfolioItemsData = [
   {
     id: 1,
-    image: IMG1,
-    title: 'Responsive Card Hover Effect',
-    tech: 'HTML, CSS, Javascript',
-    github: 'https://github.com/bhupendrachouhan19',
-    desc: 'This is a small but really cool project I made while exploring the power of pure CSS3',
-    demo: 'https://github.com/bhupendrachouhan19' 
+    image: IMG7,
+    title: 'Citizen-centric FIR System Prototype on Blockchain',
+    tech: ' Next.JS, Tailwind CSS, ThirdWeb, Solidity, Polygon Mumbai Testnet',
+    github: 'https://github.com/Bhupendrachouhan19/Citizen-centric-FIR-system-Prototype-using-blockchain',
+    desc: 'This is an e-FIR system prototype which leverage blockchain and smart contract to address the challenges in the traditional FIR system, enhancing security, transparency,corruption and efficiency in crime reporting and investigations',
+    demo: 'https://github.com/Bhupendrachouhan19/Citizen-centric-FIR-system-Prototype-using-blockchain' 
   },
   {
     id: 2,
@@ -28,8 +29,19 @@ const portfolioItemsData = [
     desc: "This project has two interface (1st) Newsletter and (2nd) Admin. User can put put join the Newsletter by entering their email adress. These email ID then stored a '.csv' file. Now, the admin from it's interface can view or download this '.csv' file' and can also write and send email to all the email ID's in that list with just One Click.",
     demo: 'https://github.com/bhupendrachouhan19' 
   },
+  
   {
     id: 3,
+    image: IMG1,
+    title: 'Responsive Card Hover Effect',
+    tech: 'HTML, CSS, Javascript',
+    github: 'https://github.com/bhupendrachouhan19',
+    desc: 'This is a small but really cool project I made while exploring the power of pure CSS3',
+    demo: 'https://github.com/bhupendrachouhan19' 
+  },
+
+  {
+    id: 4,
     image: IMG3,
     title: 'ARP Spoofing in SDN',
     tech: 'Wireshark, Mininet, P4 Programming',
@@ -38,7 +50,7 @@ const portfolioItemsData = [
     demo: 'https://github.com/bhupendrachouhan19' 
   },
   {
-    id: 4,
+    id: 5,
     image: IMG4,
     title: 'Creative Product Page (UI/UX)',
     tech: 'Figma',
@@ -47,7 +59,7 @@ const portfolioItemsData = [
     demo: 'https://github.com/bhupendrachouhan19' 
   },
   {
-    id: 5,
+    id: 6,
     image: IMG5,
     title: 'Sport T-Shirt Design',
     tech: 'Adobe Illustrator',
@@ -56,7 +68,7 @@ const portfolioItemsData = [
     demo: 'https://github.com/bhupendrachouhan19' 
   },
   {
-    id: 6,
+    id: 7,
     image: IMG6,
     title: 'Blood Donation Camp Poster',
     tech: 'Adobe Photoshop',
